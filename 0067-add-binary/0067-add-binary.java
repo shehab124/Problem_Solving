690838d2fed48b95cf0b7cb3e84f87aa74e2c56c
shehab124 <79970898+shehab124@users.noreply.github.com>
2023-10-09 23:15:04 +0300

    Time: 1 ms (100%), Space: 41.1 MB (73.06%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,53 +1,29 @@
 class Solution {
     public String addBinary(String a, String b) 
     {
-
         StringBuilder result = new StringBuilder();
-        boolean carry = false;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
 
-        if(a.length() < b.length())
+        while(i >= 0 || j >= 0)
         {
-            int size = b.length() - a.length();
-            for(int i = 0; i < size; i++)
-                a = "0" + a;
-        }
-        if(a.length() > b.length())
-        {
-            int size = a.length() - b.length();
-            for(int i = 0; i < size; i++)
-                b = "0" + b;
-        }
+            int sum = carry;
+            if(i >= 0)
+                sum += a.charAt(i) - '0';
+            if(j >= 0)
+                sum += b.charAt(j) - '0';
 
-        for(int i = a.length() - 1 ; i >= 0; i--)
-        {
-            // 0+0+0
-            if(a.charAt(i) == '0' && b.charAt(i) == '0' && carry == false)
-                result.append('0');
-            // 1+0+0
-            else if(a.charAt(i) == '0' && b.charAt(i) == '0' && carry == true)
-            {
-                result.append('1');
-                carry = false;
-            }
-            else if( (a.charAt(i) == '1' && b.charAt(i) == '0' && carry == false) || (a.charAt(i) == '0' && b.charAt(i) == '1' && carry == false) )
-                result.append('1');
-            // 1+1+0
-            else if((a.charAt(i) == '1' && b.charAt(i) == '1' && carry == false) || (a.charAt(i) == '1' && b.charAt(i) == '0' && carry == true) || (a.charAt(i) == '0' && b.charAt(i) == '1' && carry == true) )
-            {
-                result.append('0');
-                carry = true;
-            }
-            // 1+1+1
-            else
-            {
-                result.append('1');
-                carry = true;
-            }
+            result.append(sum % 2);
+            carry = sum / 2;
+
+            i--;
+            j--;
         }
-        if(carry == true)
-            result.append('1');
+        if(carry == 1)
+            result.append(1);
 
-        return result.reverse().toString();    
 
+        return result.reverse().toString();
     }
 }
